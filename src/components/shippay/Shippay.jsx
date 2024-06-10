e8f903c1e04647029887d9f66ff1298b53c7f93c
Islombek Umirov <islombek.umirov30@gmail.com>
2024-06-10 12:00:48 +0500

    done

--- src/components/shippay/Shippay.jsx
+++ src/components/shippay/Shippay.jsx
@@ -28,7 +28,7 @@ function Shippay() {
             <p>Any order can be picked up yourself at the address: Moscow, Dmitrovskoe highway, 100c2</p>
         </div>
     </div>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d191757.505580995!2d69.29678025605018!3d41.326024408251214!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae8b9cdd04953d%3A0xf8109d550917a88e!2sChorsu%20Bazaar!5e0!3m2!1sen!2s!4v1717849573020!5m2!1sen!2s" style={{width: 1300, height:600, borderRadius: 15,border: 0}} ></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d191757.505580995!2d69.29678025605018!3d41.326024408251214!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae8b9cdd04953d%3A0xf8109d550917a88e!2sChorsu%20Bazaar!5e0!3m2!1sen!2s!4v1717849573020!5m2!1sen!2s" className='shippay__map' ></iframe>
     </>
   )
 }
